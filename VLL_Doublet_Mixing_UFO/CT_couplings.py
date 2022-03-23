84c2b3b72ec7ffaece404c511f69804f90c31a40
Nazlim Agaras <nazlimagaras@nazlims.dyndns.cern.ch>
2022-03-23 14:34:14 +0100

    doublet

---
+++ VLL_Doublet_Mixing_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 9.0 for Linux x86 (64-bit) (February 7, 2013)
+# Date: Fri 28 Feb 2020 16:27:26
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
