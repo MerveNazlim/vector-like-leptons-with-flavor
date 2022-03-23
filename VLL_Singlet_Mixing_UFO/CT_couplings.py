dfc6fa659afc76ef94df61ba71584a009f6305af
Nazlim Agaras <nazlimagaras@nazlims.dyndns.cern.ch>
2022-03-23 14:31:31 +0100

    modeils

---
+++ VLL_Singlet_Mixing_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.32
+# Mathematica version: 9.0 for Linux x86 (64-bit) (February 7, 2013)
+# Date: Wed 22 Jan 2020 09:39:38
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
